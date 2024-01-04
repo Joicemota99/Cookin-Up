5b6e7574c24fa212509220d5c82acb71cbb5a60c
Joice Mota <joicemota98@hotmail.com>
2024-01-04 10:16:14 -0300

    Passando para proxima pagina

--- src/components/BotaoPrincipal.vue
+++ src/components/BotaoPrincipal.vue
@@ -23,5 +23,13 @@ export default {
     box-shadow: 4px 4px 15px 0px rgba(255, 115, 76, 0.25);
     cursor: pointer;
    transition: 0.2s;
+
+   display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.botao-principal:hover {
+  background: var(--ocre-hover, #D1451E);
 }
 </style>
