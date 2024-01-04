22093682986a6f41cc328bf7372efd5ba5c01d38
Joice Mota <joicemota98@hotmail.com>
2024-01-04 07:09:58 -0300

    Aplicando o Props

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import type ICategoria from '@/interfaces/ICategoria';
+
+export default {
+    props:{
+        categoria: {type: Object as PropType<ICategorias>, required:true}
+    }
+}
+</script>
+
+<template>
+    {{ categoria.nome }}
+    {{ categoria.imagem }}
+</template>
